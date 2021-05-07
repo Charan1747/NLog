f691f63df2cb813c272ac646f3d55575e0ea24af
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-07 15:28:31 -0700

    [Charan] Add: NLogImplementation

---
+++ NlogImplementation/NlogImplementation/AddNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NlogImplementation
+{
+    public class AddNumbers
+    {
+        Nlog nlog = new Nlog();
+        public int Sum(int a, int b)
+        {
+            if(a == 0 || b == 0)
+            {
+                nlog.LogDebug("Debug Successfull : sum()");
+                nlog.LogError("Expecting Null Value");
+                nlog.LogWarn(" a or b Should not Equal to 0");
+            }
+            int c = a + b;
+            nlog.LogDebug("Debug Succesful");
+            return c;
+        }
+    }
+}
---
+++ NlogImplementation/NlogImplementation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NlogImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adding problem\n Enter a, b Values");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            AddNumbers add = new AddNumbers();
+            Console.WriteLine("Result is" + " " + add.Sum(a,b));
+        }
+    }
+}
